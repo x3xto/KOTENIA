a22ff5cd694d87fc30dcb9bc8be2dd296c018be0
x3xto <gogomgomych@gmail.com>
2025-05-21 02:23:13 +0300

    Created example_config.py

---
+++ config_example.py
@@ -0,0 +1,6 @@
+"""
+This file is created as template. To use the script properly rename config_example.py into config.py.
+Also don't forget to paste your API keys.
+"""
+
+api_key="your-api-key-here" #https://ai.google.dev/gemini-api/docs
